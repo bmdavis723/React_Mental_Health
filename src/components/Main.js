05cd79ef41f1116fb19f6ba138e515136feb7116
Whitney Lubin <whitneylubin@icloud.com>
2018-06-27 14:51:16 -0400

    deleted "import hotline" from Main.js"

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Articles from './Articles';
-import Hotline from './Hotline';
+
 
 export default class Main extends React.Component {
  render(){
